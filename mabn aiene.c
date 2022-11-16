b814d256339f1548b02f64f3611ef7080984830e
Abtin Badiee <abtinbadie81@gmail.com>
2022-11-16 17:08:46 +0330

    version 3

--- mabn aiene.c
+++ mabn aiene.c
@@ -10,26 +10,7 @@ int main(void){
     //printf("please enter your heigh in meter:\n");
     scanf("%f",&heigh_in_meters);
 
-    float BMI=(weigh_in_kg)/(heigh_in_meters*heigh_in_meters);
 
-    printf("%.4f",BMI);
-    printf("\n");
-   
-    if (BMI<18.5) {
-        printf("Underweight\n");
-    }
-
-    if ((BMI>=18.5) && (BMI<=24.9)) {
-        printf("NORMAL\n");
-    }
-
-    if ((BMI>=25) && (BMI<=29.9)) {
-        printf("Overweigh\n");
-    }
-
-    if (BMI>=30) {
-        printf("Obese\n");
-    }
 
 
     return 0;
